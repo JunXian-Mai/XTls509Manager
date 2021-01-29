a512bd0f8e1c058dab6b1c790b3a2bbc38b1a61c
maijunxian <markensic@outlook.com>
2021-01-29 10:39:09 +0800

    验证重构后证书管理器

--- src/main/java/app/ApacheHttp.kt
+++ src/main/java/app/ApacheHttp.kt
@@ -2,6 +2,8 @@ package app
 
 import bj.anydef.tls.cert.manager.AnydefX509TrustManager
 import org.apache.http.HttpHost
+import org.apache.http.HttpServerConnection
+import org.apache.http.client.HttpClient
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.message.BasicHttpRequest
@@ -21,12 +23,12 @@ class ApacheHttp {
       """.trimIndent())
     }
 
-    fun testEmqxCert() {
-      val host = HttpHost("127.0.0.1", 8883, "https")
+    fun emqxTLSLink() {
       val sslContext = SSLContext.getInstance("TLSv1.2")
       sslContext.init(null, arrayOf(AnydefX509TrustManager()), null)
       val httpClient = HttpClientBuilder.create().setSSLContext(sslContext).build()
-      val response = httpClient.execute(host, BasicHttpRequest("GET", "\\"))
+      val httpGet = HttpGet("https://127.0.0.1:8883/")
+      val response = httpClient.execute(httpGet)
       println("""
         statusLine: ${response.statusLine}
       """.trimIndent())
@@ -34,7 +36,7 @@ class ApacheHttp {
 
     @JvmStatic
     fun main(args: Array<String>) {
-      doGet()
+      emqxTLSLink()
     }
   }
 }
